d2ab23a3765605ebfdb547ffa340a35270b9209d
mfredenberg <fredenbe20@up.edu>
2018-02-25 16:34:08 -0800

    Created UnoGameState Contructor and filled it

--- app/src/main/java/com/example/papoutsa20/gamestate/UnoGameState.java
+++ app/src/main/java/com/example/papoutsa20/gamestate/UnoGameState.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by fredenbe20 on 2/25/2018.
  */
 
-public class UnoGameState {
+public class UnoGameState{
     //Different groupings of cards
     ArrayList<Card> drawPile = new ArrayList<Card>();
     ArrayList<Card> discardPile = new ArrayList<Card>();
@@ -36,4 +36,58 @@ public class UnoGameState {
     boolean gameDirection; //true = clockwise; false = counterclockwise
 
 
+    public UnoGameState(){
+        //filling the drawPilw with Cards
+        for(int i = 0; i < 108; i++){
+            drawPile.add(i,new Card());
+        }
+        //dealing 7 cards to each player from the top of the deck in traditional fasion
+        for(int i = 0; i < 7; i++){
+            for(int j = 1; j <= numOfPlayers; ){
+                if(j == 1){
+                    player1Hand.add(drawPile.get(0));
+                    drawPile.remove(0);
+                }
+                else if(j == 2){
+
+                    player2Hand.add(drawPile.get(0));
+                    drawPile.remove(0);
+                }
+                else if(j == 3){
+                    player3Hand.add(drawPile.get(0));
+                    drawPile.remove(0);
+                }
+                else if(j == 4){
+                    player4Hand.add(drawPile.get(0));
+                    drawPile.remove(0);
+                }
+            }
+        }
+
+        //setting all of the integer-based info
+        turn = 0;
+        cardsInDraw = drawPile.size();
+        cardsInDiscard = discardPile.size();
+        numOfPlayers = 4;
+        numPlayer1Cards = player1Hand.size();
+        numPlayer2Cards = player2Hand.size();
+        numPlayer3Cards = player3Hand.size();
+        numPlayer4Cards = player4Hand.size();
+
+        //setting game direction clockwise
+        gameDirection = true;
+
+        //setting the player names
+        player1Name = "";
+        player2Name = "";
+        player3Name = "";
+        player4Name = "";
+
+        //setting the first turn player
+        currentPlayer = player1Name;
+
+        //moving the top card from drawPile to discard
+        discardPile.add(0, drawPile.get(0));
+        drawPile.remove(0);
+    }
 }
