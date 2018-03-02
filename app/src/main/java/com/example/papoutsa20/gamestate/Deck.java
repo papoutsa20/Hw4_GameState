39d26a2fb9c2ae290227dcaf3d6199ce274c5f4b
papoutsa20 <35584110+papoutsa20@users.noreply.github.com>
2018-03-02 01:05:35 -0800

    added deckV2

--- app/src/main/java/com/example/papoutsa20/gamestate/Deck.java
+++ app/src/main/java/com/example/papoutsa20/gamestate/Deck.java
@@ -1,6 +1,7 @@
 package com.example.papoutsa20.gamestate;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by steli on 3/2/2018.
@@ -10,40 +11,60 @@ public class Deck {
     private ArrayList<Card> deck = new ArrayList<Card>();
 
     public Deck(boolean isEmpty) {
-     if(!isEmpty) {
-         String[] str = {"Reverse", "Draw two", "Wild", "Wild draw 2", "Skip"};
-         for(String specialCard: str)
-         {
-             deck.add(new Card(-1,specialCard,"Red"));
-             deck.add(new Card(-1,specialCard,"Yellow"));
-             deck.add(new Card(-1,specialCard,"Green"));
-             deck.add(new Card(-1,specialCard,"Blue"));
-         }
+        if (!isEmpty) {
+            String[] str = {"Reverse", "Draw two", "Wild", "Wild draw 2", "Skip"};
+            for (String specialCard : str) {
+                deck.add(new Card(-1, specialCard, "Red"));
+                deck.add(new Card(-1, specialCard, "Yellow"));
+                deck.add(new Card(-1, specialCard, "Green"));
+                deck.add(new Card(-1, specialCard, "Blue"));
+                deck.add(new Card(-1, specialCard, "Red"));
+                deck.add(new Card(-1, specialCard, "Yellow"));
+                deck.add(new Card(-1, specialCard, "Green"));
+                deck.add(new Card(-1, specialCard, "Blue"));
+            }
 
-         for(int i = 0; i < 10; i++)
-         {
-             deck.add(new Card(i,null,"Red"));
-             deck.add(new Card(i,null,"Yellow"));
-             deck.add(new Card(i,null,"Green"));
-             deck.add(new Card(i,null,"Blue"));
-             deck.add(new Card(i,null,"Red"));
-             deck.add(new Card(i,null,"Yellow"));
-             deck.add(new Card(i,null,"Green"));
-             deck.add(new Card(i,null,"Blue"));
-             if(i != 0)
-             {
-                 deck.add(new Card(i,null,"Red"));
-                 deck.add(new Card(i,null,"Yellow"));
-                 deck.add(new Card(i,null,"Green"));
-                 deck.add(new Card(i,null,"Blue"));
-             }
+            for (int i = 0; i < 10; i++) {
+                deck.add(new Card(i, null, "Red"));
+                deck.add(new Card(i, null, "Yellow"));
+                deck.add(new Card(i, null, "Green"));
+                deck.add(new Card(i, null, "Blue"));
+                if (i != 0) {
+                    deck.add(new Card(i, null, "Red"));
+                    deck.add(new Card(i, null, "Yellow"));
+                    deck.add(new Card(i, null, "Green"));
+                    deck.add(new Card(i, null, "Blue"));
+                }
 
-         }
+            }
+            this.suffle();
 
-     }
+        }
     }
 
 
+    public void suffle() {
+        for (int i = 0; i < 500; i++) {
+            Random rand = new Random();
+            int index = rand.nextInt(108);
+            Card card = deck.get(index);
+            deck.remove(index);
+            deck.add(card);
+        }
+    }
+
+    public void put(Card card)
+    {
+        put(card,0);
+    }
+    public void put(Card card, int index)
+    {
+        this.deck.add(index,card);
+    }
+    public Card take(int index)
+    {
+        return deck.remove(index);
+    }
 
 
 }
