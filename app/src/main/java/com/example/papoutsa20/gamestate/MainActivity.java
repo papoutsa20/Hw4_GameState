4be7f1e96b3cabdf57cad7fee8d16cda7343a7ee
papoutsa20 <35584110+papoutsa20@users.noreply.github.com>
2018-03-02 15:54:10 -0800

    onclick

--- app/src/main/java/com/example/papoutsa20/gamestate/MainActivity.java
+++ app/src/main/java/com/example/papoutsa20/gamestate/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.papoutsa20.gamestate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText tv;
 
@@ -13,26 +14,35 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button test = (Button) findViewById(R.id.buttonTest);
+        test.setOnClickListener(this);
+        this.tv = (EditText) findViewById(R.id.TestText);
+    }
 
     public void onClick(View v) {
         if (v.getId() == R.id.buttonTest) {
             this.tv.setText("");
+
             UnoGameState firstInstance = new UnoGameState();
-            UnoGameState secondInstance = new UnoGameState(firstInstance,1);
+            UnoGameState secondInstance = new UnoGameState(firstInstance, 1);
+
             firstInstance.drawCard(firstInstance.getCurrentPlayer());
             this.tv.setText("Player 1 has drawn a card\n");
-            firstInstance.placeCard(firstInstance.getCurrentPlayer(),firstInstance.getCurrentPlayerHand().get(0));
+
+            firstInstance.placeCard(firstInstance.getCurrentPlayer(), firstInstance.getCurrentPlayerHand().get(0));
             this.tv.setText(this.tv.getText() + "Player 1 has placed a card\n");
             this.tv.setText(this.tv.getText() + "does player 1 have uno?" +
-                    firstInstance.hasUno(firstInstance.currentPlayer)+"\n");
+                    firstInstance.hasUno(firstInstance.currentPlayer) + "\n");
+
             firstInstance.skipTurn(firstInstance.getCurrentPlayer());
             this.tv.setText(this.tv.getText() + "Player 1 has skipped their turn!!\n");
+
             UnoGameState thirdInstance = new UnoGameState();
-            UnoGameState fourthInstance = new UnoGameState(thirdInstance,1);
+            UnoGameState fourthInstance = new UnoGameState(thirdInstance, 1);
             this.tv.setText(this.tv.getText() + "Tostring of second instance!" + secondInstance.toString()
-                    +"\n");
+                    + "\n");
             this.tv.setText(this.tv.getText() + "Tostring of fourth instance!" + fourthInstance.toString()
-                    +"\n");
+                    + "\n");
 
 
         }
@@ -40,5 +50,5 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     }
 
 
-    }
 }
+
