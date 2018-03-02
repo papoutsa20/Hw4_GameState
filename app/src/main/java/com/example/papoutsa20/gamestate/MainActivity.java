8498e0a708207bb958fc8566cc23789fb5f41e83
papoutsa20 <35584110+papoutsa20@users.noreply.github.com>
2018-03-02 15:22:41 -0800

    mainAct

--- app/src/main/java/com/example/papoutsa20/gamestate/MainActivity.java
+++ app/src/main/java/com/example/papoutsa20/gamestate/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.papoutsa20.gamestate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonTest) {
+            this.tv.setText("");
+            UnoGameState firstInstance = new UnoGameState();
+
+
+        }
+
+    }
 
 
     }
